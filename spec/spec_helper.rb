c6bfa3a2e951784bb99cea6b03700cd45da2ba12
Ryo Nakamura <r7kamura@gmail.com>
2015-03-29 02:44:49 +0900

    Use RSpec for testing but no tests written for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,14 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'serverkit'
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.disable_monkey_patching!
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  config.warnings = true
+end
