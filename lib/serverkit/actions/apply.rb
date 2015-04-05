391613607ce0e048f510c1fb828f96d75d690d5d
Ryo Nakamura <r7kamura@gmail.com>
2015-04-06 06:33:17 +0900

    Use threads for multiple backends

--- lib/serverkit/actions/apply.rb
+++ lib/serverkit/actions/apply.rb
@@ -1,21 +1,24 @@
 require "serverkit/actions/base"
+require "thread"
 
 module Serverkit
   module Actions
     class Apply < Base
       def run
         backends.map do |backend|
-          recipe.resources.each do |resource|
-            resource.backend = backend
-            if resource.check
-              puts "[SKIP] #{resource.id} on #{host_for(backend)}"
-            else
-              resource.apply
-              result = resource.check ? "DONE" : "FAIL"
-              puts "[#{result}] #{resource.id} on #{host_for(backend)}"
+          Thread.new do
+            recipe.resources.map(&:dup).each do |resource|
+              resource.backend = backend
+              if resource.check
+                puts "[SKIP] #{resource.id} on #{host_for(backend)}"
+              else
+                resource.apply
+                result = resource.check ? "DONE" : "FAIL"
+                puts "[#{result}] #{resource.id} on #{host_for(backend)}"
+              end
             end
           end
-        end
+        end.each(&:join)
       end
     end
   end
--- lib/serverkit/actions/check.rb
+++ lib/serverkit/actions/check.rb
@@ -1,16 +1,19 @@
 require "serverkit/actions/base"
+require "thread"
 
 module Serverkit
   module Actions
     class Check < Base
       def run
         backends.map do |backend|
-          recipe.resources.each do |resource|
-            resource.backend = backend
-            result = resource.check ? "OK" : "NG"
-            puts "[ #{result} ] #{resource.id} on #{host_for(backend)}"
+          Thread.new do
+            recipe.resources.map(&:dup).each do |resource|
+              resource.backend = backend
+              result = resource.check ? "OK" : "NG"
+              puts "[ #{result} ] #{resource.id} on #{host_for(backend)}"
+            end
           end
-        end
+        end.each(&:join)
       end
     end
   end
