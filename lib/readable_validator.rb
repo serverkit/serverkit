0afed94cd75563bf788effca6ba0075c6458e574
Ryo Nakamura <r7kamura@gmail.com>
2015-04-06 05:23:12 +0900

    Fix readable validator on nil value case

--- lib/readable_validator.rb
+++ lib/readable_validator.rb
@@ -2,7 +2,7 @@ require "active_model"
 
 class ReadableValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless File.readable?(value)
+    if !value.nil? && !File.readable?(value)
       record.errors.add(attribute, "can't be unreadable path")
     end
   end
