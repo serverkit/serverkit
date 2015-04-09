47fefbad87f099c8c23377a04316a07875e9644b
Ryo Nakamura <r7kamura@gmail.com>
2015-04-10 02:43:16 +0900

    Add check_script and recheck_script to command resource

--- lib/serverkit/resources/command.rb
+++ lib/serverkit/resources/command.rb
@@ -3,7 +3,9 @@ require "serverkit/resources/base"
 module Serverkit
   module Resources
     class Command < Base
+      attribute :check_script, type: String
       attribute :script, required: true, type: String
+      attribute :recheck_script, type: String
 
       # @note Override
       def apply
@@ -12,12 +14,29 @@ module Serverkit
 
       # @note Override
       def check
-        false
+        if check_script
+          check_command(check_script)
+        else
+          false
+        end
+      end
+
+      private
+
+      def default_id
+        script
       end
 
       # @note Override
       def recheck
-        true
+        case
+        when recheck_script
+          check_command(recheck_script)
+        when check_script
+          check_command(check_script)
+        else
+          true
+        end
       end
     end
   end
