9c3cdcb85eaf366fec494c34f5b752a21a16fe90
Ryo Nakamura <r7kamura@gmail.com>
2015-04-12 01:57:20 +0900

    Support ssh_options injection via constructor arguments
    e.g. `Serverkit::Actions::Apply.new(ssh_options: ..., ...)`

--- lib/serverkit/actions/base.rb
+++ lib/serverkit/actions/base.rb
@@ -11,10 +11,12 @@ module Serverkit
     class Base
       # @param [String, nil] hosts
       # @param [String] recipe_path
+      # @param [Hash, nil] ssh_options For override default ssh options
       # @param [Stirng, nil] variables_path
-      def initialize(hosts: nil, recipe_path: nil, variables_path: nil)
+      def initialize(hosts: nil, recipe_path: nil, ssh_options: nil, variables_path: nil)
         @hosts = hosts
         @recipe_path = recipe_path
+        @ssh_options = ssh_options
         @variables_path = variables_path
       end
 
@@ -31,7 +33,7 @@ module Serverkit
 
       # @return [Array<Specinfra::Backend::Base>]
       def backends
-        if @hosts
+        if has_hosts?
           hosts.map do |host|
             backend_class.new(
               disable_sudo: true,
@@ -47,7 +49,7 @@ module Serverkit
       end
 
       def backend_class
-        if @hosts
+        if has_hosts?
           Specinfra::Backend::Ssh
         else
           Specinfra::Backend::Exec
@@ -59,13 +61,25 @@ module Serverkit
       rescue Bundler::GemfileNotFound
       end
 
+      def has_hosts?
+        !@hosts.nil?
+      end
+
+      # @return [Array<String>, nil]
       def hosts
-        @options[:hosts].split(",")
+        if has_hosts?
+          @hosts.split(",")
+        end
       end
 
       def setup
         bundle
         abort_with_errors unless recipe.valid?
+      end
+
+      # @return [Hash]
+      def ssh_options
+        @ssh_options || {}
       end
 
       # @return [Serverkit::Recipe]
