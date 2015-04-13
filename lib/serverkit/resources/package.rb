26d4b5d2e478528ef2a8e175ff4f4fddb6770af1
Ryo Nakamura <r7kamura@gmail.com>
2015-04-13 15:09:52 +0900

    Support options and version attributes on package resource

--- lib/serverkit/resources/package.rb
+++ lib/serverkit/resources/package.rb
@@ -4,15 +4,17 @@ module Serverkit
   module Resources
     class Package < Base
       attribute :name, required: true, type: String
+      attribute :options, type: String
+      attribute :version, type: String
 
       # @note Override
       def apply
-        run_command_from_identifier(:install_package, name)
+        run_command_from_identifier(:install_package, name, version, options)
       end
 
       # @note Override
       def check
-        check_command_from_identifier(:check_package_is_installed, name)
+        check_command_from_identifier(:check_package_is_installed, name, version)
       end
 
       private
