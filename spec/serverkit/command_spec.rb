c2d0c464a706369f8366deae25e0a521325e4625
Ryo Nakamura <r7kamura@gmail.com>
2015-04-27 00:31:56 +0900

    Add test case

---
+++ spec/serverkit/command_spec.rb
@@ -0,0 +1,28 @@
+RSpec.describe Serverkit::Command do
+  let(:command) do
+    described_class.new(argv)
+  end
+
+  describe "#call" do
+    subject do
+      command.call
+    end
+
+    context "with validate as 1st argument" do
+      let(:argv) do
+        ["validate", "recipe.yml"]
+      end
+
+      it "calls Serverkit::Actions::Validate action" do
+        expect(Serverkit::Actions::Validate).to receive(:new).with(
+          hash_including(
+            log_level: Logger::INFO,
+            recipe_path: "recipe.yml",
+          )
+        ).and_call_original
+        expect_any_instance_of(Serverkit::Actions::Validate).to receive(:call)
+        subject
+      end
+    end
+  end
+end
