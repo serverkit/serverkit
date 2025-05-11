fa21aad0314553b9fb40c7f31f197c821ba79fd5
Toshimaru <me@toshimaru.net>
2025-05-11 12:13:19 +0900

    test: Test `Serverkit::Resources::User`'s attributes

--- spec/serverkit/resources/user_spec.rb
+++ spec/serverkit/resources/user_spec.rb
@@ -2,13 +2,23 @@
 
 RSpec.describe Serverkit::Resources::User do
   let(:recipe) { Serverkit::Recipe.new(resources: [attributes]) }
-  let(:attributes) do
-    {
-      "type" => "user",
-    }
-  end
 
   subject { described_class.new(recipe, attributes) }
+
+  describe "attributes" do
+    let(:attributes) do
+      { "type" => "user" }
+    end
+
+    it { expect(subject.type).to eq "user" }
+    it { is_expected.to respond_to :gid }
+    it { is_expected.to respond_to :home }
+    it { is_expected.to respond_to :name }
+    it { is_expected.to respond_to :password }
+    it { is_expected.to respond_to :shell }
+    it { is_expected.to respond_to :system }
+    it { is_expected.to respond_to :uid }
+  end
 
   describe "system" do
     context "system is true" do
